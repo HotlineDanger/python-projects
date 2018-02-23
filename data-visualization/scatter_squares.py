16978438e7ed74d7d9d7f0cf9f8564d57ed5481f
Thibault Smetryns <tsmetryns@corero.com>
2018-02-23 17:08:57 +0000

    python-dev -- data-visualization - removing edgecolor

--- data-visualization/scatter_squares.py
+++ data-visualization/scatter_squares.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 x_values = list(range(1, 1001))
 y_values = [x ** 2 for x in x_values]
 
-plt.scatter(x_values, y_values, s = 100)
+plt.scatter(x_values, y_values, s = 10, edgecolor = "none")
 
 # Set chart title and axis labels
 plt.title("Square Numbers", fontsize = 24)
