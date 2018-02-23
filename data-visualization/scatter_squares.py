f924f15fe0c08988aa606fbee3b5ed8ca4071d5b
Thibault Smetryns <tsmetryns@corero.com>
2018-02-23 16:33:18 +0000

    python-dev -- data-visualization - playing with list_comprehension ofr generating the data

--- data-visualization/scatter_squares.py
+++ data-visualization/scatter_squares.py
@@ -1,7 +1,10 @@
 import matplotlib.pyplot as plt
 
-x_values = [1, 2, 3, 4, 5]
-y_values = [1, 4, 9, 16, 25]
+# x_values = [1, 2, 3, 4, 5]
+# y_values = [1, 4, 9, 16, 25]
+# Loop for generating data
+x_values = list(range(1, 1001))
+y_values = [x ** 2 for x in x_values]
 
 plt.scatter(x_values, y_values, s = 100)
 
@@ -11,6 +14,7 @@ plt.xlabel("Value", fontsize = 14)
 plt.ylabel("Square of values", fontsize = 14)
 
 # Set size of the tick labels
-plt.tick_params(axis="both", which='major', labelsize = 14)
-
+# The axis() function requires four values: the minimum and maximum values for the x-axis and
+# the y-axis. Here, we run the x-axis from 0 to 1100 and the y-axis from 0 to 1,100,000
+plt.axis([0, 1100, 0, 1100000])
 plt.show()
