49f7e4107eab293eb7980956e5ab48f13735caf0
Thibault Smetryns <tsmetryns@corero.com>
2018-03-06 13:20:21 +0000

    python-dev -- data-visualization - plotting points with gradient

--- data-visualization/rw_visual.py
+++ data-visualization/rw_visual.py
@@ -15,14 +15,18 @@ while True:
     rw = RandomWalk(50000)
     rw.fill_walk()
 
+    # Set the size of the plotting window so it fits nicely on the screen.
+    plt.figure(figsize = (10, 6))
+
     point_numbers = list(range(rw.num_points))
     # Coloring the points
     point_numbers = list(range(rw.num_points))
     plt.scatter(rw.x_values, rw.y_values, c = point_numbers, cmap = plt.cm.Blues, edgecolor = 'none', s = 1)
 
-    '''# plotting the starting and ending point
+    # plotting the starting and ending point
     plt.scatter(0, 0, c = 'green', edgecolors = 'none', s = 100)
-    plt.scatter(rw.x_values[-1], rw.y_values[-1], c = 'red', cmap = plt.cm.Blues, edgecolors = 'none', s = 100)'''
+    plt.scatter(rw.x_values[-1], rw.y_values[-1], c = 'red', cmap = plt.cm.Blues, edgecolors = 'none', s = 100)
+
     # Removing the axis
     plt.axes().get_xaxis().set_visible(False)
     plt.axes().get_yaxis().set_visible(False)
