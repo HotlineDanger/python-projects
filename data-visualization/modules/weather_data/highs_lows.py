7aa8988bd447bf6c7ab5fb982fb80af8c50cddcb
Thibault Smetryns <tsmetryns@corero.com>
2018-03-19 12:05:39 +0000

    python-dev -- data-visualization - plotting highest temperature using matplotlib

--- data-visualization/modules/weather_data/highs_lows.py
+++ data-visualization/modules/weather_data/highs_lows.py
@@ -1,9 +1,30 @@
 import csv
+from matplotlib import pyplot as plt
 
 filename = 'sitka_weather_07-2014.csv'
+
+# Get high temperatures
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
 
-    for index, column_header in enumerate(header_row):
-        print(index, column_header)
+    highs = []
+    for row in reader:
+        # We convert the string to number to be read with matplotlib
+        high = int(row[1])
+        highs.append(high)
+
+# for index, column_header in enumerate(header_row):
+#     print(index, column_header)
+
+ # Plot data.
+fig = plt.figure(dpi = 128, figsize = (10, 6))
+plt.plot(highs, c = 'red')
+
+# Format plot.
+plt.title('Daily high temperatures, July 2014', fontsize = 24)
+plt.xlabel('', fontsize = 16)
+plt.ylabel('Temperature (F)', fontsize = 16)
+plt.tick_params(axis = 'both', which = 'major', labelsize = 16)
+
+plt.show()
