a5484ddce83e9e37baf96ebc8c8686cae111ae34
Thibault Smetryns <tsmetryns@corero.com>
2018-04-17 17:27:37 +0100

    python-dev -- learning-logs - progress on templates

--- learning-log/learning_logs/views.py
+++ learning-log/learning_logs/views.py
@@ -8,4 +8,13 @@ def index(request):
     return render(request, 'learning_logs/index.html')
 
 def topics(request):
-    """ . """
+    """ Show all topics. """
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+
+    return render(request, 'learning_logs/topics.html', context)
+    # We first import the model associated with the data we need u. The topics() function needs one parameter: the request object Django received
+    # from the server v. At w we query the database by asking for the Topic objects, sorted by the date_added attribute. We store the resulting queryset
+    # in topics. At x we define a context that we’ll send to the template. A context is a dictionary in which the keys are names we’ll use in the template to access
+    # the data and the values are the data we need to send to the template. In this case, there’s one key-value pair, which contains the set of topics we’ll display
+    # on the page. When building a page that uses data, we pass the context variable to render() as well as the request object and the path to the template y.
