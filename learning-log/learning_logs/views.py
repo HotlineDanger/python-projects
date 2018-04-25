fa7d77a2395cefc2cc943726d7223a2731d8a948
Thibault Smetryns <tsmetryns@corero.com>
2018-04-25 17:31:20 +0100

    python-dev - learning_logs - add a new entry progress

--- learning-log/learning_logs/views.py
+++ learning-log/learning_logs/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 
 from .models import Topic
-from .forms import TopicForm
+from .forms import TopicForm, EntryForm
 # Create your views here.
 
 def index(request):
@@ -26,7 +26,7 @@ def topic(request, topic_id):
     """ Show a single topic and all its entries. """
     topic = Topic.objects.get(id = topic_id)
     entries = topic.entry_set.order_by('-date_added') # - to sort in reverse order
-    context = {'topic': topic, 'entries': entries}
+    context = { 'topic': topic, 'entries': entries }
 
     return render(request, 'learning_logs/topic.html', context)
     # The function accepts the value captured by the expression (?P<topic_id>\d+) and stores it in topic_id
@@ -41,9 +41,32 @@ def new_topic(request):
         form = TopicForm(request.POST)
         if form.is_valid():
             form.save()
+
             return HttpResponseRedirect(reverse('topics'))
             # The reverse() function determines the URL from a named URL pattern, meaning that
             # Django will generate the URL when the page is requested. We also import the form we just wrote, TopicForm.
 
-    context = {'form': form}
+    context = { 'form': form }
     return render(request, 'learning_logs/new_topic.html', context)
+
+def new_entry(request, topic_id):
+    """ Add a new entry for a particular topic. """
+    topic = Topic.objects.get(id = topic_id)
+
+    if request.method != 'POST':
+        # No data submitted create a blank form
+        form = EntryForm()
+    else:
+        # Post data submitted and process data
+        form = EntryForm(data = request.POST)
+
+        if form.is_valid():
+            new_entry = form.save(commit = False)
+            new_entry.topic = topic
+            mew_entry.save()
+
+            return HttpResponseRedirect(reverse('topic', args=[topic_id]))
+
+    context = { 'topic': topic, 'form': form }
+    return render(request, 'learning_logs/new_entry.html', context)
+    
