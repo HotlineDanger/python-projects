242b8d229708f6da69c275af6bf2a8b49358327c
Thibault Smetryns <tsmetryns@corero.com>
2018-05-01 17:25:21 +0100

    python-dev - learning_logs - register user progress

--- learning-log/users/views.py
+++ learning-log/users/views.py
@@ -1,9 +1,30 @@
 from django.http import HttpResponseRedirect
 from django.urls import reverse
-from django.contrib.auth import logout
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render
 
-def logout_view(request):
+def logout(request):
     """ Log the user out. """
     logout(request)
     return HttpResponseRedirect(reverse('index'))
+
+def register():
+    """ Register a new user. """
+    if request.method != 'POST':
+        # Display a blank registration form
+        form = UserCreationForm()
+    else:
+        # Process the completed form
+        form = UserCreationForm(data = request.POST)
+
+        if form.is_valid():
+            new_user = form.save()
+            # Log the user in and then redirect to homepage
+            authenticated_user = authenticate(username = new_user.username, password = request.POST['password1'])
+            login(request, authenticated_user)
+
+            return HttpResponseRedirect(reverse('index'))
+
+    context = {'form': form}
+    return render(request, 'users/register.html', context)
