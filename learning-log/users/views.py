8d85b12e9f03bb9b1dd78df4def1dc20caf4f451
Thibault Smetryns <tsmetryns@corero.com>
2018-05-01 12:25:20 +0100

    python-dev -- learning_logs - log out progress

--- learning-log/users/views.py
+++ learning-log/users/views.py
@@ -1,3 +1,9 @@
+from django.http impot HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.contrib.auth import logout
 from django.shortcuts import render
 
-# Create your views here.
+def logout_view(request):
+    """ Log the user out. """
+    logout(request)
+    return HttpResponseRedirect(reverse('learning_logs:index'))
